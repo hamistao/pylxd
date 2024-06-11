27c573e8ad030aea5c6c8d0e9612221975d26b7c
hamistao <pedro.ribeiro@canonical.com>
2024-06-11 18:42:25 -0300

    integration/test_storage: Remove unused imports
    Signed-off-by: hamistao <pedro.ribeiro@canonical.com>

--- integration/test_storage.py
+++ integration/test_storage.py
@@ -12,8 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import random
-import string
 import unittest
 
 import pylxd.exceptions as exceptions
